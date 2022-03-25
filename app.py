1226b6ed426f41f8c688a14b912facf1e85d9e2f
Malliha25 <96416052+Malliha25@users.noreply.github.com>
2022-03-25 11:41:18 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from flask import Flask, render_template,request
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+app=Flask(__name__)
+
+dataset=pd.read_csv('50_startups_MLR.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
+ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[3])],remainder='passthrough')
+X=ct.fit_transform(X)
+reg=LinearRegression()
+reg.fit(X,Y)
+@app.route('/')
+def homePage():
+    return(render_template('index.html'))
+
+@app.route('/predict',methods=['POST'])
+def collectData():
+    rd=float(request.form['rd'])
+    a=float(request.form['a'])
+    m=float(request.form['m'])
+    s=request.form['s']
+    print(rd,a,m,s)
+    b1=0
+    b2=0
+    b3=0
+    if(s.lower()=='florida'):
+        b1=0
+        b2=1
+        b3=0
+    elif(s.lower()=='new york'):
+        b1=0
+        b2=0
+        b3=1
+    elif(s.lower()=='california'):
+        b1=1
+        b2=0
+        b3=0
+    out=reg.predict([[b1,b2,b3,rd,a,m]])[0]
+    return(render_template('index.html',result=out))
+
+if (__name__=="__main__"):
+    app.run()
---
+++ main.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from flask import Flask,request
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+app=Flask(__name__)
+
+dataset=pd.read_csv('50_startups_MLR.csv')
+X=dataset.iloc[:,:-1].values
+Y=dataset.iloc[:,-1].values
+ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[3])],remainder='passthrough')
+X=ct.fit_transform(X)
+reg=LinearRegression()
+reg.fit(X,Y)
+@app.route('/predict',methods=['GET'])
+def getData():
+    rd=request.args.get('rd')
+    a=request.args.get('a')
+    m=request.args.get('m')
+    s=request.args.get('s')
+    print(rd,a,m,s)
+    b1=0
+    b2=0
+    b3=0
+    if(s.lower()=='florida'):
+        b1=0
+        b2=1
+        b3=0
+    elif(s.lower()=='new york'):
+        b1=0
+        b2=0
+        b3=1
+    elif(s.lower()=='california'):
+        b1=1
+        b2=0
+        b3=0
+    out=reg.predict([[b1,b2,b3,rd,a,m]])[0]
+    return (str(out))
+if (__name__=='__main__'):
+    app.run()
